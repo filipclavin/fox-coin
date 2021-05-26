f107e27b256a06b7e87be98da1d75324d7d67da8
Melinda <melinda.sava.td@gmail.com>
2021-05-26 10:38:53 +0200

    added global styes and navbar first variant

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import {useState, useEffect} from 'react';
+import { createGlobalStyle } from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
 
+const GlobalStyle = createGlobalStyle`
+h1{
+  font-family: 'Quicksand', sans-serif;
+}
+body{
+  font-family: 'Quicksand', sans-serif;
+}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
+  const [navToggleClicked, setNavToggleClicked] = useState(false);
+
+  const toggleNav = () => setNavToggleClicked(!navToggleClicked);
+
+   const handleScroll = () => setNavToggleClicked(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [])
+
   return (
     <div className="App">
-
+    <h1>Hello there</h1>
+    <Router>
+        <Navbar navToggleClicked={navToggleClicked} toggleNav={toggleNav}/>
+          <Switch>
+            <Route path="/projects">
+              {/* <Projects /> */}
+            </Route>
+            <Route path="/Contact">
+              {/* <Contact /> */}
+            </Route>
+            <Route path="/">
+              {/* <Home /> */}
+            </Route>
+          </Switch>
+      </Router>       
+    <GlobalStyle />
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Nav = styled.nav`
+display: block;
+z-index: 1;
+div {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  position: fixed;
+  top: 30px;
+  right: 30px;
+  border-radius: 50%;
+  background-color: #C28449;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 4;
+}
+div span {
+  display: block;
+  width: 33px;
+  height: 4px;
+  margin-bottom: 5px;
+  background: #fff;
+  border-radius: 3px;
+  z-index: 4;
+}
+div span:last-child {
+  margin-bottom: 0;
+}
+.clicked span:first-child {
+  transform: translateY(9px) rotate(45deg);
+}
+.clicked span:nth-child(2) {
+  transform: translateX(-50px);
+  opacity: 0;
+}
+.clicked span:last-child {
+  transform: translateY(-9px) rotate(-45deg);
+}
+ul {
+  position: fixed;
+  list-style: none;
+  text-align: left;
+  width: 50vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  padding: 50px;
+  padding-top: 100px;
+  padding-left: 15%;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  
+  background: #fff;
+  -webkit-font-smoothing: antialiased;
+  /* to stop flickering of text in safari */
+
+  z-index: 3;
+  
+  transform-origin: 0% 0%;
+  transform: translate(-100%, 0);
+  @media screen and (min-width: 1024px ) {
+      left: auto;
+      right: 0;
+      height: auto;
+      padding-left: 50px;
+      display: flex;
+      justify-content: space-around;
+      transform: translate(100%, 0);
+  }
+}
+ul a {
+    font-weight: 600;
+    text-decoration: none;
+    color: #000;
+}
+ul li {
+  font-size: 22px;
+  padding: 25px 0;
+  span {
+  font-family: 'Quicksand', sans-serif;
+    border-top:3px solid #C28449;
+  }
+}
+.clicked ~ ul {
+    transform: translate(0, 0);
+}
+`
+
+const Navbar = ({navToggleClicked, toggleNav}) => {
+  return (
+  <section>
+    <Nav>
+      <div className={navToggleClicked ? "clicked" : ""} onClick={toggleNav} >
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
+      <ul className="top-nav" id="js-menu">
+          <Link to="/" onClick={toggleNav}><li className="menu-item"><span>.Home</span></li></Link>
+          <Link to="/projects" onClick={toggleNav}><li className="menu-item"><span>.Projects</span></li></Link>
+          <Link to="/contact" onClick={toggleNav}><li id="contact" className="menu-item"><span>.Contact</span></li></Link>
+      </ul>       
+    </Nav>
+  </section>
+  )
+}
+
+export default Navbar;
