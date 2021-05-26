07d35535673eb352e97e9542f831fbf2d95c57a2
filipclavin <filip.clavin@student.kyh.se>
2021-05-26 14:55:08 +0200

    added some margin over team

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,4 @@
-import Jesper from '../../resources/Jesper.jpg';
+import Jesper from '../../resources/jesper.jpg';
 import Filip from '../../resources/filip.jpg';
 import Melinda from '../../resources/melinda.png';
 import styled from 'styled-components';
@@ -8,6 +8,7 @@ const StyledFooter = styled.div`
 width: 100%;
 display: flex;
 flex-direction: column;
+margin-top: 50px;
 `
 
 const Team = styled.section`
@@ -46,32 +47,32 @@ h2{
 
 const Footer = () => {
   return (
-  <StyledFooter>
-    <Team>
-    <h2>The team.</h2>
-    <div className="team">
-      <div className="team-member">
-        <p className="name">Jesper Sjöström</p>
-        <p>Guru (Developer)</p>
-        <a href="https://github.com/SjostromJesper"><img className="profile" src={Jesper} alt="Jesper"/>
-        </a>
-      </div>
-      <div className="team-member">
-        <p className="name">Filip Calvin</p>
-        <p>Hacker (General Developer)</p>
-        <a href="https://github.com/filipclavin"><img className="profile" src={Filip} alt="Filip"/>
-        </a>
-      </div>
-      <div className="team-member">
-        <p className="name">Melinda Sava</p>
-        <p>Kick Ass Developer (Developer)</p>
-        <a href="https://github.com/mellyynda"><img className="profile" src={Melinda} alt="Melinda"/></a>
-      </div>
-    </div>
-    </Team>
-    <section>
-    </section>
-  </StyledFooter>
+    <StyledFooter>
+      <Team>
+        <h2>The team.</h2>
+        <div className="team">
+          <div className="team-member">
+            <p className="name">Jesper Sjöström</p>
+            <p>Guru (Developer)</p>
+            <a href="https://github.com/SjostromJesper"><img className="profile" src={Jesper} alt="Jesper" />
+            </a>
+          </div>
+          <div className="team-member">
+            <p className="name">Filip Calvin</p>
+            <p>Hacker (General Developer)</p>
+            <a href="https://github.com/filipclavin"><img className="profile" src={Filip} alt="Filip" />
+            </a>
+          </div>
+          <div className="team-member">
+            <p className="name">Melinda Sava</p>
+            <p>Kick Ass Developer (Developer)</p>
+            <a href="https://github.com/mellyynda"><img className="profile" src={Melinda} alt="Melinda" /></a>
+          </div>
+        </div>
+      </Team>
+      <section>
+      </section>
+    </StyledFooter>
   )
 }
 
