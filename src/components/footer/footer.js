5cd71e97554eeff8ebcf96ea26332f87f27cfb25
Melinda <melinda.sava.td@gmail.com>
2021-05-26 12:58:18 +0200

    initialized footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+  <section>
+    <Social> 
+      <h2></h2>
+    </Social>
+
+    <Team>
+    </Team>
+    <Contact>
+    </Contact>
+  </section>
+  )
+}
+
+export default Footer;
