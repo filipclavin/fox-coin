9c32ee06188d87093bd2ae0da801197a3e8afdc2
Melinda <melinda.sava.td@gmail.com>
2021-05-26 14:41:28 +0200

    fixed team

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,6 @@
-import Jesper from '../../resources/Jesper.jpg';
+import Jesper from '../../resources/jesper.jpg';
+import Filip from '../../resources/filip.jpg';
+import Melinda from '../../resources/melinda.png';
 import styled from 'styled-components';
 import github from '../../resources/github-square.svg';
 
@@ -14,18 +16,32 @@ h2{
  width: 100%;
  text-align: center;
 }
-.team-member{
+.team{
+  display:flex;
+  justify-content:space-around;
+  flex-direction: column;
+  @media screen and (min-width: 1024px ){
+    flex-direction: row;
+  }
+  .team-member{
   display: flex;
   flex-direction: column;
   align-items:center;
+  margin-top: 30px;
+  .name{
+    font-size: 35px;
+    font-weight:300;
+  }
   .profile{
     max-width:200px;
     border-radius: 50%;
   }
   a img{
-    width:30px;
+    max-width:200px;
   }
 }
+}
+
 `
 
 const Footer = () => {
@@ -35,10 +51,21 @@ const Footer = () => {
     <h2>The team.</h2>
     <div className="team">
       <div className="team-member">
-        <p>Jesper Sjöström</p>
+        <p className="name">Jesper Sjöström</p>
         <p>Guru (Developer)</p>
-        <img className="profile" src={Jesper} alt="Jesper"/>
-        <a href="https://github.com/SjostromJesper"><img src={github} alt="github link"/></a>
+        <a href="https://github.com/SjostromJesper"><img className="profile" src={Jesper} alt="Jesper"/>
+        </a>
+      </div>
+      <div className="team-member">
+        <p className="name">Filip Calvin</p>
+        <p>Hacker (General Developer)</p>
+        <a href="https://github.com/filipclavin"><img className="profile" src={Filip} alt="Filip"/>
+        </a>
+      </div>
+      <div className="team-member">
+        <p className="name">Melinda Sava</p>
+        <p>Kick Ass Developer (Developer)</p>
+        <a href="https://github.com/mellyynda"><img className="profile" src={Melinda} alt="Melinda"/></a>
       </div>
     </div>
     </Team>
